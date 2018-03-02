5641d70cb4cd50829182fa072a712bf929bab26f
duongtdn <duongtdn@gmail.com>
2018-03-02 20:04:01 +0700

    local is used for dev only

--- src/signup.local.js
+++ src/signup.local.js
@@ -1,23 +1,30 @@
 "use strict"
-const PORT = 3001;
 
 const { app, userdb } = require('./signup.app');
 
 const dynamodb = require('@stormgle/userdb-dynamodb')
 
-userdb.use( dynamodb({
-  region : 'us-west-2', 
-  endpoint : 'http://localhost:8000'}
+
+/* add dynamodb middleware to userdb-api */
+userdb.use( dynamodb(
+  {
+    region : 'us-west-2', 
+    endpoint : `${process.env.DB_HOST}:${process.env.DB_PORT}`
+  },
+  (err) => {
+    if (err) {
+      console.log('Failed to init local db')
+      console.log(err)
+    } else {
+      const httpServer = require('http').createServer(app);
+      const PORT = process.env.POST_SIGNUP;
+      httpServer.listen(PORT)
+      console.log(`#Signup service is running at localhost:${PORT}\n`);
+    }
+  }
 ));
 
-const httpServer = require('http').createServer(app);
 
-/* only for dev and test purpose, should not in production */
-app.post('/close', (res, req) => {
-  httpServer.close();
-  process.exit(0);
-})
 
-httpServer.listen(PORT)
 
-console.log(`Signup service is running at localhost:${PORT}\n`);
+
