8582d6b93c7c1f47fe262177dcddd426846c4ad8
duongtdn <duongtdn@gmail.com>
2018-03-05 19:50:55 +0700

    remove all functions in returned user object

--- src/serializer.js
+++ src/serializer.js
@@ -4,6 +4,11 @@ function serializeUser (req, res, next) {
   delete req.user.uid
   delete req.user.login
   delete req.user.policies
+  for (let props in req.user) {
+    if (typeof req.user[props] === 'function') {
+      delete req.user[props]
+    }
+  }
   next();
 }
 
