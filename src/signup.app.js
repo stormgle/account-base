63f0ba7a13648c686c2d6d42e6b0595d3b73551f
duongtdn <duongtdn@gmail.com>
2018-03-06 15:10:04 +0700

    just bertter code format

--- src/signup.app.js
+++ src/signup.app.js
@@ -5,11 +5,11 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser');
 
+const UserDB = require('@stormgle/userdb-api');
+
 const uuid = require('uuid/v1');
 const { serializeUser } = require('./lib/serializer');
 const { generateToken } = require('./lib/token');
-
-const UserDB = require('@stormgle/userdb-api');
 
 const userdb = new UserDB();
 const app = express();
