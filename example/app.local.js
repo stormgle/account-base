22bd5c5b43e096f67baf49f0ea684be3840b7596
duongtdn <duongtdn@gmail.com>
2018-03-13 10:33:15 +0700

    add example for dev on localhost

---
+++ example/app.local.js
@@ -0,0 +1,32 @@
+"use strict"
+
+const app = require('../src/app')
+
+const dynamodb = require('@stormgle/userdb-dynamodb')
+
+const dbDriver = dynamodb({ 
+  region : 'us-west-2', 
+  endpoint : `${process.env.DB_HOST}:${process.env.DB_PORT}`
+});
+
+const funcs = [
+  'user/signup',
+  'user/login',
+  'user/update/profile',
+  'user/update/password',
+  'admin/query/user',
+  'admin/update/user'
+]
+
+app
+  .useDbDriver(dbDriver)
+
+funcs.forEach(func => {
+  app.createFunction(`/${func}`, require(`../src/${func}.app`))
+})  
+
+const PORT = 3100;
+const httpServer = require('http').createServer(app);
+httpServer.listen(PORT)
+console.log(`# user-services is running at localhost:${PORT}\n`);
+  
