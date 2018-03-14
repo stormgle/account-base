b9ef70679127af451a633fa4142553663de39471
duongtdn <duongtdn@gmail.com>
2018-03-14 09:31:17 +0700

    setup will download dynamodb-local ifnot installed

--- scripts/setup.js
+++ scripts/setup.js
@@ -4,15 +4,31 @@ const fs = require('fs');
 
 // remove .env file, then create new one
 if (fs.existsSync('./.env')) {
-  console.log('Removing old .env file...')
+  console.log('Removing old [.env] file...')
   fs.unlinkSync('.env');
 }
 
-console.log('Generating new .env file...');
+console.log('Generating new [.env] file...');
 
 ['.keys', '.env.test'].forEach( (env) => {
   fs.createReadStream(`./env/${env}`)
     .pipe(fs.createWriteStream('.env', {'flags': 'a'}));
 })
 
-console.log('Setup completed')
+require('dotenv').config()
+
+// download dynamo-db local
+const DynamoDbLocal = require('dynamodb-local');
+
+DynamoDbLocal.configureInstaller({
+  installPath: './localdb',
+  downloadUrl: 'https://s3.eu-central-1.amazonaws.com/dynamodb-local-frankfurt/dynamodb_local_latest.tar.gz'
+});
+
+DynamoDbLocal.launch(process.env.DB_PORT, null, ['shareDb'])
+  .then(function() {
+    console.log('DynamoDB is installed and ready to use')
+    DynamoDbLocal.stop(process.env.DB_PORT);
+    console.log('Setup completed')
+  })
+
