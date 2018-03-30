2db1f68997b1fed091517204c6e8346b3b55ade9
duongtdn <duongtdn@gmail.com>
2018-03-30 15:33:56 +0700

    add forgot_password

---
+++ api/post/auth/forgot_password.js
@@ -0,0 +1,47 @@
+"use strict"
+
+const jwt = require('jsonwebtoken')
+
+const secret = process.env.DELIGATE_KEY_FORGOT_PASSWORD;
+
+function generateToken(userdb, { onSuccess, onFailure }) {
+  return function(req, res, next) {
+    const email = req.body.email;
+    if (!email) {
+      res.status(400).json({error: 'Bad request: must specify email'});
+      onFailure && onFailure({error: 'Bad request: must specify email'})
+    }
+    
+    userdb.queryUser(
+      { username: email },
+      (err, user) => {
+        if (err) {
+          res.status(500).json({error: 'Internal error: Access database error'});
+          onFailure && onFailure({error: 'Internal error: Access database error'})
+        } else {
+          if (user && user.uid) {
+            const token = jwt.sign(
+              {uid: user.uid}, 
+              secret,
+              { expiresIn: "1 days"}
+            );
+            req.token = token;
+            next();
+          } else {
+            res.status(400).json({error: 'Bad request: must specify email'});
+            onFailure && onFailure({error: 'Bad request: email is npot registered'});
+          }
+        }
+      }
+    )
+  }
+}
+
+function final(userdb, { onSuccess, onFailure }) {
+  return function(req, res) {
+    res.status(200).json({email: req.body.email});
+    onSuccess && onSuccess({token: req.token});
+  }
+}
+
+module.exports = [generateToken, final]
