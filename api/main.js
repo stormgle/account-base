4b6ce0ee74a2193d8de5a190731973f8bbde8f39
duongtdn <duongtdn@gmail.com>
2018-06-15 06:08:52 +0700

    wrap the functions generation to allow passing option to api functions

--- api/main.js
+++ api/main.js
@@ -2,55 +2,46 @@
 
 const api = require('express-api-binder')
 
-const funcs = [
-  'post/auth/signup',
-  'post/auth/login',
-  'post/me/update_profile',
-  'post/me/update_password',
-  'post/users/update',
-  'get/users/:username',
-];
+api.generateFunctions = ({forgotPassword, form, reset}) => {
+  
+  const funcs = [
+    'post/auth/signup',
+    'post/auth/login',
+    'post/me/update_profile',
+    'post/me/update_password',
+    'post/users/update',
+    'get/users/:username',
+  ];
+  
+  funcs.forEach(func => {
+    const { method, uri, includePath } = api.parseApi(func);
+    api.createFunction(method, uri, require(`./${includePath}`))
+  }) 
+  
+  api.createFunction(
+    'post', 
+    '/auth/forgot_password', 
+    require('./post/auth/forgot_password'), 
+    forgotPassword
+  )
+  
 
-funcs.forEach(func => {
-  const { method, uri, includePath } = api.parseApi(func);
-  api.createFunction(method, uri, require(`./${includePath}`))
-}) 
+  api.createFunction(
+    'get', 
+    '/auth/0/form', 
+    require('./get/auth/0/form'), 
+    form
+  )
+  
+  api.createFunction(
+    'post', 
+    '/auth/reset_password', 
+    require('../api/post/auth/reset_password'), 
+    reset
+  )
 
-api.createFunction(
-  'post', 
-  '/auth/forgot_password', 
-  require('./post/auth/forgot_password'), 
-  {
-    onSuccess: (token) => console.log(token.token),
-    onFailure: (err) => console.log(err)
-  }
-)
-
-const PORT = 3100;
-api.createFunction(
-  'get', 
-  '/auth/0/form', 
-  require('./get/auth/0/form'), 
-  {
-    title: 'Auth-0', 
-    body:'Hello from Auth-0 / ',
-    endPoint: `http://localhost:${PORT}/auth/reset_password`,
-    redirect: {
-      success: `http://localhost:${PORT}/`
-    }
-  }
-)
-
-api.createFunction(
-  'post', 
-  '/auth/reset_password', 
-  require('../api/post/auth/reset_password'), 
-  {
-    title: 'Auth-0', 
-    service: 'Example',
-    redirect: `http://localhost:${PORT}/`
-  }
-)
+  return this;
+}
 
 
 module.exports = api;
--- example/app.local.js
+++ example/app.local.js
@@ -17,7 +17,30 @@ userdb.use(dynamodb({
   endpoint : `${process.env.DB_HOST}:${process.env.DB_PORT}`
 }));
 
+const PORT = process.env.PORT_LOCAL_TEST || 3100;
+
+const forgotPassword = {
+  onSuccess: (token) => console.log(token.token),
+  onFailure: (err) => console.log(err)
+};
+
+const form = {
+  title: 'Auth-0', 
+  body:'Hello from Auth-0 / ',
+  endPoint: `http://localhost:${PORT}/auth/reset_password`,
+  redirect: {
+    success: `http://localhost:${PORT}/`
+  }
+};
+
+const reset = {
+  title: 'Auth-0', 
+  service: 'Example',
+  redirect: `http://localhost:${PORT}/`
+}
+
 api.useDatabase({ userdb })
+   .generateFunctions({forgotPassword, form, reset});
 
 const app = express();
 
